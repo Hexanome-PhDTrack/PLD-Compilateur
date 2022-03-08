0805cefd24b192cab502beb620adeed74ce90738
onyr <rascoussier.florian@gmail.com>
2022-03-08 10:31:04 +0100

    Added getters and setters to VarData.

--- compiler/variable/VarData.h
+++ compiler/variable/VarData.h
@@ -2,6 +2,15 @@
 #include "variable/TypeName.h"
 
 class VarData {
+
+private:
+    int index;
+    std::string varName;
+    std::string lineContext;
+    size_t lineNumber = 0;
+    bool isUsed = false;
+    TypeName typeName;
+    
 public:
     int index;
     std::string varName;
@@ -28,7 +37,38 @@ public:
     inline void WitnessUsage() { isUsed = true; }
     inline bool IsUsed() { return isUsed; }
 
-private:
-    bool isUsed = false;
+    // getters
+    inline int GetIndex() {
+        return index;
+    }
+    inline std::string GetVarName() {
+        return varName; 
+    }
+    inline std::string GetLineContext() {
+        return lineContext;
+    }
+    inline size_t GetLineNumber() {
+        return lineNumber;
+    }
+    inline TypeName GetTypeName() {
+        return typeName;
+    }
+
+    // setters
+    inline void SetIndex(int index) {
+        this->index = index;
+    }
+    inline void SetVarName(std::string varName) {
+        this->varName = varName;
+    }
+    inline void SetLineContext(std::string lineContext) {
+        this->lineContext = lineContext;
+    }
+    inline void SetLineNumber(size_t lineNumber) {
+        this->lineNumber = lineNumber;
+    }
+    inline void SetTypeName(TypeName typeName) {
+        this->typeName = typeName;
+    }
 
 };
