b5799df2ce5b3000e9afd6bb914c223877c850a3
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-03-22 10:14:56 +0100

    Implementation of gen_asm and documentation

--- inc/main/ir/instruction/SubInstr.h
+++ inc/main/ir/instruction/SubInstr.h
@@ -8,10 +8,18 @@
 #include <string>
 #include <iostream>
 
-class SubInstr : public IRInstr{
-    public:
-        SubInstr(Block* bb_, TypeName t, std::vector<std::string> params): IRInstr(bb, t, params) {};
+class SubInstr : public IRInstr
+{
+public:
+    /**
+     * @brief Construct a new Sub Instr object
+     *
+     * @param bb_
+     * @param t
+     * @param params
+     * var1 = var2 - var3; => first parameter -> var1 | second parameter -> var2 | third parameter -> var3
+     */
+    SubInstr(Block *bb_, TypeName t, std::vector<std::string> params) : IRInstr(bb, t, params){};
 
-        virtual void gen_asm(std::ostream &o);
-
+    virtual void gen_asm(std::ostream &o);
 };
