d68a774f02aafaeba94b65c699fae115dec52a7f
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-03-22 09:07:20 +0100

    Begin of implementation of ControlFlowGraph

--- src/main/ir/flow/ControlFlowGraph.cpp
+++ src/main/ir/flow/ControlFlowGraph.cpp
@@ -1,5 +1,46 @@
 #include "ir/flow/ControlFlowGraph.h"
 
-ControlFlowGraph::ControlFlowGraph() {
-    
+ControlFlowGraph::ControlFlowGraph()
+{
+}
+
+ControlFlowGraph::~ControlFlowGraph()
+{
+    for (int i = 0; i < this->blocks.size(); i++)
+    {
+        delete this->blocks[i];
+    }
+}
+
+void ControlFlowGraph::add_bb(Block *bb)
+{
+    this->blocks.push_back(bb);
+}
+
+void ControlFlowGraph::gen_asm(std::ostream &o)
+{
+    // TODO
+}
+
+std::string ControlFlowGraph::IR_reg_to_asm(std::string reg)
+{
+    VarData var = this->variableManager.getVariable(reg);
+    int index = var.GetIndex();
+    std::string asm_result = index + "(%rbp)";
+    return asm_result;
+}
+
+void ControlFlowGraph::gen_asm_prologue(std::ostream &o)
+{
+    // TODO
+}
+
+void ControlFlowGraph::gen_asm_epilogue(std::ostream &o)
+{
+    // TODO
+}
+
+std::string ControlFlowGraph::new_BB_name()
+{
+    // TODO
 }
