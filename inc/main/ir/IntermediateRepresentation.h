20da56ccc477e52886ae384d903b0145c0c8cc12
fitz35 <clement@lahoche.info>
2022-03-22 09:09:25 +0100

    add function management

--- inc/main/ir/IntermediateRepresentation.h
+++ inc/main/ir/IntermediateRepresentation.h
@@ -5,18 +5,28 @@
 #include "ir/flow/CallData.h"
 
 #include <vector>
+#include <map>
 
 class IntermediateRepresentation {
 private:
-    std::vector<Function*> functions; 
+    std::map<std::string, Function*> functions; // WARNING : don't delete this
+    std::vector<Function*> functionsToDelete; // delete this
+
     std::vector<CallData*> calls; // deleted elsewhere
 
 public:
     IntermediateRepresentation() {}
     ~IntermediateRepresentation();
 
-    void AddFunction(Function* function);
+    void AddFunction(std::string name, Function* function);
 
     void AddCall(CallData* call);
 
+    /**
+     * @brief Get the Function object by the name
+     * 
+     * @param name the name
+     * @return Function* 
+     */
+    Function * getFunction(std::string name);
 };
