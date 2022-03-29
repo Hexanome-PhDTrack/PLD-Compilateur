b0f09384e34b1869bfc3659105642d64c7dec9bb
fitz35 <clement@lahoche.info>
2022-03-29 08:42:26 +0200

    add some functionnalities on cfg

--- inc/main/ir/flow/ControlFlowGraph.h
+++ inc/main/ir/flow/ControlFlowGraph.h
@@ -25,7 +25,7 @@ public:
 
     // x86 code generation: could be encapsulated in a processor class in a retargetable compiler
     void gen_asm(std::ostream& o);
-    std::string IR_reg_to_asm(std::string reg); /**< helper method: inputs a IR reg or input variable, returns e.g. "-24(%rbp)" for the proper value of 24 */
+    std::string IR_reg_to_asm(std::string name); /**< helper method: inputs a IR reg or input variable, returns e.g. "-24(%rbp)" for the proper value of 24 */
     void gen_asm_prologue(std::ostream& o);
     void gen_asm_epilogue(std::ostream& o);
 
@@ -34,6 +34,7 @@ public:
     std::string create_new_tempvar(TypeName t);
     bool isExist(std::string name);
     TypeName get_var_type(std::string name);
+    bool removeTempVariable(std::string varName);
 
     // basic block management
     std::string new_BB_name();
