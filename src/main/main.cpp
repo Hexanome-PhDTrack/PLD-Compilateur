e13d345ae55389ea60f3f0a64c753524d8f6e570
fitz35 <clement@lahoche.info>
2022-03-29 11:07:27 +0200

    adapt main

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -15,14 +15,26 @@ using namespace std;
 
 int main(int argn, const char **argv) {
     stringstream in;
+    std::filebuf targetFileBuffer;
     if (argn==2)
     {
         ifstream lecture(argv[1]);
         in << lecture.rdbuf();
+
+        std::string targetName(std::regex_replace(argv[1], std::regex(".c$"), ".s"));
+        targetFileBuffer.open(targetName,std::ios::out);
+    }
+    else if (argn == 3)
+    {
+        ifstream lecture(argv[1]);
+        in << lecture.rdbuf();
+    
+        std::string targetName(argv[2]);
+        targetFileBuffer.open(targetName,std::ios::out);
     }
     else
     {
-        cerr << "usage: ifcc path/to/file.c" << endl ;
+        cerr << "usage: ifcc path/to/file.c path/to/file.s" << endl ;
         exit(1);
     }
     
@@ -44,5 +56,19 @@ int main(int argn, const char **argv) {
 
     Visitor v;
     int success = v.visit(tree).as<int>();
+
+    if(targetFileBuffer.is_open()){
+        std::ostream os(&targetFileBuffer);
+        v.getFunction()->getControlFlowGraph()->gen_asm(os);
+    }
+    else{
+        std::ostream os(std::cout.rdbuf());
+        v.getFunction()->getControlFlowGraph()->gen_asm(os);
+    }
+    
+    if(targetFileBuffer.is_open()){
+        targetFileBuffer.close();
+    }
+    
     return success;
 }
