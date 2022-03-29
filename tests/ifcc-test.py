a3348f04d83bc88f2547ba71d9ab50163ae8af8b
Oskar Wallgren <oskarwallgren@icloud.com>
2022-03-29 09:17:04 +0200

    added success rate to test script

--- tests/ifcc-test.py
+++ tests/ifcc-test.py
@@ -5,12 +5,12 @@
 # input: the test-cases are specified either as individual
 #         command-line arguments, or as part of a directory tree
 #
-# output: 
+# output:
 #
 # The script is divided in three distinct steps:
 # - in the ARGPARSE step, we understand the command-line arguments
 # - in the PREPARE step, we copy all our test-cases into a single directory tree
-# - in the TEST step, we actually run GCC and IFCC on each test-case 
+# - in the TEST step, we actually run GCC and IFCC on each test-case
 # test
 #
 
@@ -23,8 +23,8 @@ import subprocess
 
 
 class bcolors:
-    OK = '\033[92m' #GREEN correct program 
-    OK1= '\033[34m' #BLUE ifcc correctly rejects invalid program 
+    OK = '\033[92m' #GREEN correct program
+    OK1= '\033[34m' #BLUE ifcc correctly rejects invalid program
     WARNING = '\033[93m' #YELLOW ifcc wrongly rejects valid program -> error
     FAIL = '\033[91m' #RED ifcc wrongly accepts invalid program -> error
     RESET = '\033[0m' #RESET COLOR
@@ -78,12 +78,12 @@ orig_cwd=os.getcwd()
 if "ifcc-test-output" in orig_cwd:
     print('error: cannot run from within the output directory')
     exit(1)
-    
+
 if os.path.isdir('ifcc-test-output'):
     # cleanup previous output directory
     command('rm -rf ifcc-test-output')
 os.mkdir('ifcc-test-output')
-    
+
 ## Then we process the inputs arguments i.e. filenames or subtrees
 inputfilenames=[]
 for path in args.input:
@@ -133,7 +133,7 @@ if not os.path.isfile(wrapper):
 
 if args.debug:
     print("debug: wrapper path: "+wrapper)
-        
+
 ######################################################################################
 ## PREPARE step: copy all test-cases under ifcc-test-output
 
@@ -146,7 +146,7 @@ for index,inputfilename in enumerate(inputfilenames):
     if 'ifcc-test-output' in os.path.realpath(inputfilename):
         print('error: input filename is within output directory: '+inputfilename)
         exit(1)
-    
+
     ## each test-case gets copied and processed in its own subdirectory:
     ## ../somedir/subdir/file.c becomes ./ifcc-test-output/somedir-subdir-file/input.c
     subdir='ifcc-test-output/'+str(index)+ "-"+inputfilename.strip("./")[:-2].replace('/','-')
@@ -171,12 +171,13 @@ if args.debug:
 ######################################################################################
 ## TEST step: actually compile all test-cases with both compilers
 
+successCounter = 0
 for jobindex, jobname in enumerate(jobs):
     os.chdir(orig_cwd)
 
     print('TEST-CASE: '+jobname)
     os.chdir(jobname)
-    
+
     ## Reference compiler = GCC
     gccstatus=command("gcc -S -o asm-gcc.s input.c", "gcc-compile.txt")
     if gccstatus == 0:
@@ -186,14 +187,15 @@ for jobindex, jobname in enumerate(jobs):
         exegccstatus=command("./exe-gcc", "gcc-execute.txt")
         if args.verbose >=2:
             dumpfile("gcc-execute.txt")
-            
+
     ## IFCC compiler
     ifccstatus=command(wrapper+" asm-ifcc.s input.c", "ifcc-compile.txt")
-    
+
     if gccstatus != 0 and ifccstatus != 0:
         ## ifcc correctly rejects invalid program -> test-case ok
-        
+
         print(bcolors.OK1 + str(jobindex) +" TEST OK "  + bcolors.RESET)
+        successCounter = successCounter + 1
 
         continue
     elif gccstatus != 0 and ifccstatus == 0:
@@ -217,7 +219,7 @@ for jobindex, jobname in enumerate(jobs):
 
     ## both compilers  did produce an  executable, so now we  run both
     ## these executables and compare the results.
-        
+
     command("./exe-ifcc","ifcc-execute.txt")
     if open("gcc-execute.txt").read() != open("ifcc-execute.txt").read() :
         print("TEST FAIL (different results at execution)")
@@ -230,3 +232,7 @@ for jobindex, jobname in enumerate(jobs):
 
     ## last but not least
     print(bcolors.OK + "TEST OK"+ bcolors.RESET)
+
+print("passed tests: " + str(successCounter) + " total tests: " + str(len(jobs)))
+successRate = round((successCounter / len(jobs)) * 100)
+print("Success rate: " + str(successRate) + '%' )
