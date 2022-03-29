27d7cb2c96bccd20eccdc0b08a4d4dc29e6f2e26
Thibaud Martin <thibaud.martin@insa-lyon.fr>
2022-03-29 11:22:59 +0200

    Implementing ALL the instructions.

--- src/main/ir/instruction/WmemInstr.cpp
+++ src/main/ir/instruction/WmemInstr.cpp
@@ -3,6 +3,5 @@
 void WmemInstr::gen_asm(std::ostream &o)
 {
 	o << "	leaq " << params.at(1).GetIndex() << "(%rbp), %rax \n";
-	o << "	movq "
-	  << "(%rax), " << params.at(0).GetIndex() << "(%rbp) \n";
+	o << "	movq " << "(%rax), " << params.at(0).GetIndex() << "(%rbp) \n";
 };
