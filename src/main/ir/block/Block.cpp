dc87a1efb064fe611b0e7cdf49c65d0c301399ea
msaug <msaug@protonmail.com>
2022-03-29 08:50:18 +0200

    Block implemented function - CFG fixed circular dependency

--- src/main/ir/block/Block.cpp
+++ src/main/ir/block/Block.cpp
@@ -1,6 +1,15 @@
 #include "ir/block/Block.h"
 #include "ir/instruction/IRInstr.h" // circular import
 
+Block::Block(ControlFlowGraph *cfg, BlockLabel entry_label, Context *ctx)
+{
+    exit_true = nullptr;
+    exit_false = nullptr;
+    this->cfg = cfg;
+    this->context = ctx;
+    label = entry_label;
+}
+
 Block::~Block() {
     for(int i = 0; i < instrs.size(); i++){
         delete instrs[i];
