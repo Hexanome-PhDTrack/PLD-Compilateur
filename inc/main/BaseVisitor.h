cb5ecc09752e539bb7ad8903ca7db81296b4c242
onyr <rascoussier.florian@gmail.com>
2022-03-30 12:30:29 +0200

    Corrections to BitXorInstr.

--- inc/main/BaseVisitor.h
+++ inc/main/BaseVisitor.h
@@ -1,15 +1,11 @@
-//
-// Created by Mathieu Saugier on 15/03/2022.
-//
-
-#ifndef PLD_COMPILATEUR_BASEVISITOR_H
-#define PLD_COMPILATEUR_BASEVISITOR_H
+#pragma once
 
 #include "./generated/ifccBaseVisitor.h"
 #include "error/ErrorManager.h"
 #include "warning/WarningManager.h"
 #include "ir/block/Block.h"
 #include "ir/flow/Function.h"
+
 //instr
 #include "ir/instruction/CopyInstr.h"
 #include "ir/instruction/LdconstInstr.h"
@@ -18,7 +14,7 @@
 #include "ir/instruction/MulInstr.h"
 #include "ir/instruction/BitAndInstr.h"
 #include "ir/instruction/BitOrInstr.h"
-#include "ir/instruction/BitXor.h"
+#include "ir/instruction/BitXorInstr.h"
 #include "ir/instruction/CmpGeInstr.h"
 #include "ir/instruction/CmpGtInstr.h"
 #include "ir/instruction/CmpNeqInstr.h"
@@ -49,5 +45,3 @@ protected:
     Function* currentFunction;
     Block* currentBlock;
 };
-
-#endif //PLD_COMPILATEUR_BASEVISITOR_H
--- src/main/ir/instruction/BitXorInstr.cpp
+++ src/main/ir/instruction/BitXorInstr.cpp
@@ -1,6 +1,6 @@
-#include "ir/instruction/BitXor.h"
+#include "ir/instruction/BitXorInstr.h"
 
-void BitAndInstr::gen_asm(std::ostream &o)
+void BitXorInstr::gen_asm(std::ostream &o)
 {
     o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
     o << "	xorl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
