4aa3691727a2bc38d72933db4ec4b32214bd7645
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-03-30 17:00:59 +0200

    Adding the multitype handler for the gen_asm of AddInstr

--- src/main/ir/instruction/AddInstr.cpp
+++ src/main/ir/instruction/AddInstr.cpp
@@ -2,10 +2,86 @@
 
 void AddInstr::gen_asm(std::ostream &o)
 {
-  o << "	movl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
-  o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
-  o << "	addl "
-    << "%edx, %eax \n";
-  o << "	movl "
-    << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+  /**
+   * There are multiple possibilities, depending of the types of the operand and of the result.
+   */
+
+  if (params.at(0).GetTypeName() == TYPE_INT and params.at(1).GetTypeName() == TYPE_INT and params.at(2).GetTypeName() == TYPE_INT)
+  {
+    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movl "
+      << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_INT and params.at(1).GetTypeName() == TYPE_INT and params.at(2).GetTypeName() == TYPE_CHAR)
+  {
+    o << "	movsbl " << params.at(2).GetIndex() << "(%rbp), %edx \n";
+    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movl "
+      << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_INT and params.at(1).GetTypeName() == TYPE_CHAR and params.at(2).GetTypeName() == TYPE_INT)
+  {
+    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movl "
+      << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_INT and params.at(1).GetTypeName() == TYPE_CHAR and params.at(2).GetTypeName() == TYPE_CHAR)
+  {
+    o << "	movsbl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "	movsbl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movl "
+      << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_CHAR and params.at(1).GetTypeName() == TYPE_INT and params.at(2).GetTypeName() == TYPE_INT)
+  {
+    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "  movl "
+      << "%eax, %edx \n";
+    o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movb "
+      << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_CHAR and params.at(1).GetTypeName() == TYPE_INT and params.at(2).GetTypeName() == TYPE_CHAR)
+  {
+    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "  movl "
+      << "%eax, %edx \n";
+    o << "	movzbl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movb "
+      << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_CHAR and params.at(1).GetTypeName() == TYPE_CHAR and params.at(2).GetTypeName() == TYPE_INT)
+  {
+    o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "  movl "
+      << "%eax, %edx \n";
+    o << "	movzbl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movb "
+      << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
+  else if (params.at(0).GetTypeName() == TYPE_CHAR and params.at(1).GetTypeName() == TYPE_CHAR and params.at(2).GetTypeName() == TYPE_CHAR)
+  {
+    o << "	movzbl " << params.at(1).GetIndex() << "(%rbp), %edx \n";
+    o << "	movzbl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+    o << "	addl "
+      << "%edx, %eax \n";
+    o << "	movb "
+      << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
+  }
 };
