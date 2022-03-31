500b97df0f0e1baf04b893ef608a3601dc9231b8
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-03-31 12:29:03 +0200

    Implementing BitAndInstr assembly generation for char and int operands

--- src/main/ir/instruction/BitAndInstr.cpp
+++ src/main/ir/instruction/BitAndInstr.cpp
@@ -19,35 +19,69 @@ void BitAndInstr::gen_asm(std::ostream &o) {
     else if (params.at(0).GetTypeName() == TYPE_INT and
              params.at(1).GetTypeName() == TYPE_INT and
              params.at(2).GetTypeName() == TYPE_CHAR) {
+        o << "	movsbl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+        o << "	andl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "	movl "
+          << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // INT = CHAR & INT
     else if (params.at(0).GetTypeName() == TYPE_INT and
              params.at(1).GetTypeName() == TYPE_CHAR and
              params.at(2).GetTypeName() == TYPE_INT) {
+        o << "	movsbl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "	andl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+        o << "	movl "
+          << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // INT = CHAR & CHAR
     else if (params.at(0).GetTypeName() == TYPE_INT and
              params.at(1).GetTypeName() == TYPE_CHAR and
              params.at(2).GetTypeName() == TYPE_CHAR) {
+        o << "	movzbl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "	andb " << params.at(2).GetIndex() << "(%rbp), %al \n";
+        o << "	movsbl "
+          << "%al, %eax \n";
+        o << "	movl "
+          << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // CHAR = INT & INT
     else if (params.at(0).GetTypeName() == TYPE_CHAR and
              params.at(1).GetTypeName() == TYPE_INT and
              params.at(2).GetTypeName() == TYPE_INT) {
+        o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "  movl "
+          << "%eax, %edx \n";
+        o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+        o << "	andl "
+          << "%edx, %eax \n";
+        o << "	movb "
+          << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // CHAR = INT & CHAR
     else if (params.at(0).GetTypeName() == TYPE_CHAR and
              params.at(1).GetTypeName() == TYPE_INT and
              params.at(2).GetTypeName() == TYPE_CHAR) {
+        o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "	andb " << params.at(2).GetIndex() << "%al \n";
+        o << "	movb "
+          << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // CHAR = CHAR & INT
     else if (params.at(0).GetTypeName() == TYPE_CHAR and
              params.at(1).GetTypeName() == TYPE_CHAR and
              params.at(2).GetTypeName() == TYPE_INT) {
+        o << "	movl " << params.at(2).GetIndex() << "(%rbp), %eax \n";
+        o << "	andb " << params.at(1).GetIndex() << "%al \n";
+        o << "	movb "
+          << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
     // CHAR = CHAR & CHAR
     else if (params.at(0).GetTypeName() == TYPE_CHAR and
              params.at(1).GetTypeName() == TYPE_CHAR and
              params.at(2).GetTypeName() == TYPE_CHAR) {
+        o << "	movzbl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+        o << "	andb " << params.at(2).GetIndex() << "%al \n";
+        o << "	movb "
+          << "%al, " << params.at(0).GetIndex() << "(%rbp) \n";
     }
 };
