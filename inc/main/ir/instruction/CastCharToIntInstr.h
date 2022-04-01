46e957deb4390c1a6cbe09b1fcf67fa697550c16
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-04-01 09:18:57 +0200

    Adding cast instructions

---
+++ inc/main/ir/instruction/CastCharToIntInstr.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ir/block/Block.h"
+#include "ir/instruction/IRInstr.h"
+#include "variable/TypeName.h"
+
+class CastCharToIntInstr : public IRInstr {
+   public:
+    /**
+     * @brief Construct a new CastCharToInt Instr object
+     *
+     * @param bb
+     * @param t
+     * @param params
+     * var1 = (char)var2; => first parameter -> var1 | second parameter -> var2
+     */
+    CastCharToIntInstr(Block *bb, TypeName t, std::vector<VarData> params)
+        : IRInstr(bb, t, params){};
+
+    virtual void gen_asm(std::ostream &o);
+};
