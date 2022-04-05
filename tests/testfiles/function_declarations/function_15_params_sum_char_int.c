368d743569cf4d673eed43e2c42bb3c04906c4a6
onyr <rascoussier.florian@gmail.com>
2022-04-05 20:27:12 +0200

    Added tests on function declaration and calls with many parameters.

---
+++ tests/testfiles/function_declarations/function_15_params_sum_char_int.c
@@ -0,0 +1,18 @@
+int function15(
+    char p1, int p2, int p3, char p4, int p5,
+    int p6, char p7, int p8, int p9, int p10,
+    int p11, int p12, int p13, char p14, int p15
+) {
+    return p1 + p2 + p3 + p4 + p5 
+        + p6 + p7 + p8 + p9 + p10 
+        + p11 + p12 + p13 + p14 + p15;
+}
+
+int main() {
+    function15(
+        97, 98, 99, 100, 101, 
+        102, 103, 104, 105, 106, 
+        107, 108, 109, 110, 111
+    );
+    return 3;
+}
