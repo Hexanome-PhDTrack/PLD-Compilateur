59f59c9334e4e42851d19ea5cfb08d00e42909c3
Hugo91160 <hugolebon@yahoo.com>
2022-04-05 15:04:38 +0200

    Small fix

--- src/main/ir/instruction/BitNotInstr.cpp
+++ src/main/ir/instruction/BitNotInstr.cpp
@@ -1,9 +1,10 @@
 #include "ir/instruction/BitNotInstr.h"
 
-void BitNotInstr::gen_asm(std::ostream &o) {
-    o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
-    o << "  notl "
-      << "%eax \n";
-    o << "	movl "
-      << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
+void BitNotInstr::gen_asm(std::ostream &o)
+{
+	o << "	movl " << params.at(1).GetIndex() << "(%rbp), %eax \n";
+	o << "  notl "
+	  << "%eax \n";
+	o << "	movl "
+	  << "%eax, " << params.at(0).GetIndex() << "(%rbp) \n";
 };
