742047168b7559c54020d76c6067fb29a2250562
Hugo91160 <hugo.le-bon@insa-lyon.fr>
2022-04-05 09:54:17 +0200

    Adding tests for char and int operations

---
+++ tests/testfiles/char_and_int_operations/addition5.c
@@ -0,0 +1,6 @@
+int main() {
+    int a = 2;
+    char b = 8;
+    int c = b + a;
+    return c;
+}
---
+++ tests/testfiles/char_and_int_operations/addition6.c
@@ -0,0 +1,6 @@
+int main() {
+    char a = 2;
+    char b = 8;
+    char c = a + b;
+    return c;
+}
